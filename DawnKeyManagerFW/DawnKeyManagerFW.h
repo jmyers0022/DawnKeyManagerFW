0b66408935c8d4d4f8d77bc5b57795215c0fea65
Jake Myers <jmyers0022@gmail.com>
2023-06-05 15:45:11 -0600

    Initial Commit

---
+++ DawnKeyManagerFW/DawnKeyManagerFW.h
@@ -0,0 +1,18 @@
+//
+//  DawnKeyManagerFW.h
+//  DawnKeyManagerFW
+//
+//  Created by jake on 6/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DawnKeyManagerFW.
+FOUNDATION_EXPORT double DawnKeyManagerFWVersionNumber;
+
+//! Project version string for DawnKeyManagerFW.
+FOUNDATION_EXPORT const unsigned char DawnKeyManagerFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DawnKeyManagerFW/PublicHeader.h>
+
+
