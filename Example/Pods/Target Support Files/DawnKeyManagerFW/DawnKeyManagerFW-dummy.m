dbb199ee0e3740afee1c5ff2d85f0224d912dd05
Jake Myers <jmyers0022@gmail.com>
2023-06-07 13:12:26 -0600

    podify

---
+++ Example/Pods/Target Support Files/DawnKeyManagerFW/DawnKeyManagerFW-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DawnKeyManagerFW : NSObject
+@end
+@implementation PodsDummy_DawnKeyManagerFW
+@end
---
+++ Example/Pods/Target Support Files/Pods-DawnKeyManagerFW-DawnKeyManagerFWTests/Pods-DawnKeyManagerFW-DawnKeyManagerFWTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DawnKeyManagerFW_DawnKeyManagerFWTests : NSObject
+@end
+@implementation PodsDummy_Pods_DawnKeyManagerFW_DawnKeyManagerFWTests
+@end
